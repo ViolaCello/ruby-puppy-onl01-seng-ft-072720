c662f65cf3eeb6c5fca2a14c8e0561cad6ad8274
Anthony Lepore <violacellonyc@gmail.com>
2020-08-04 20:24:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 # Add your code here
 class Dog 
   @@all = []
-  @@names = []
+  
   
   attr_accessor :name
   
   def initialize(name)
     @name = name
-    @@names.push(name)
+   
     @@all.push(self)
   end
   
